9fda7489dfc89daeb5bd87749ba169b1e9bf2466
Megan Gilpin <megangilpin@gmail.com>
2020-06-15 11:20:18 -0400

    SpeechSynthesis

--- WebCam/scripts.js
+++ WebCam/scripts.js
@@ -27,9 +27,9 @@ function paintToCanvas() {
     //take the pixels out
     let pixels = ctx.getImageData(0, 0, width, height);
     // add the effect
-    // pixels = rgbSplit(pixels);
+    pixels = rgbSplit(pixels);
     // ctx.globalAlpha = 0.8;
-      // pixels = greenScreen(pixels);
+    // pixels = greenScreen(pixels);
     // put them back
     ctx.putImageData(pixels, 0, 0)
 
